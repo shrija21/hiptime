0f0f2bd7b1385f5a1b8f1aa6f8ac3f9b0f575bc5
shri <shrija.tumma1994@gmail.com>
2020-07-11 21:58:05 +0530

    User_id to Items Table and Associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200711150239) do
+ActiveRecord::Schema.define(version: 20200711161238) do
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
